ab7e954f4cc7e6e579ef68230325f250f7d54ad7
Tejas Bavaskar <tejkar777@gmail.com>
2021-09-12 13:57:32 +0530

    added search/photos endpoint

--- server/middleware/searchPhotos.js
+++ server/middleware/searchPhotos.js
@@ -1,3 +1,17 @@
+import { ifError } from "assert";
+import { AxiosApiRequest } from "../services/axiosRequest.js";
+
 export async function searchPhotos(req, res, next) {
-  
+  const fields = {
+    query: req.query.query,
+    page: req.query.page,
+  };
+
+  try {
+    const response = await AxiosApiRequest.get("search/photos", fields);
+    res.status(response.status).send(response.data);
+  } catch (err) {
+    console.log("Exception= ", err);
+    next(err);
+  }
 }
--- server/services/axiosRequest.js
+++ server/services/axiosRequest.js
@@ -1,9 +1,27 @@
-import axios  from "axios";
+import axios from "axios";
 
 export class AxiosApiRequest {
-  async get(params) {
-    const response = await axios({
+  static async get(url, fields) {
+    const accessKey = process.env.ACCESS_KEY;
+    const baseUrl = process.env.UNSPLASH_BASE_URL;
+    const method = "get";
+    const apiUrl = `${baseUrl}${url}`;
 
+    const searchWord = fields.query || "";
+    const page = fields.page || 1;
+
+    const response = await axios({
+      url: apiUrl,
+      method: method,
+      headers: {
+        authorization: accessKey,
+      },
+      params: {
+        query: searchWord,
+        page: page,
+      },
     });
+
+    return response;
   }
 }
